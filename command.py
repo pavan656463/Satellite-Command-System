2777c458a0b8e4e9a9a6e63578320645d6817007
pavan656463 <pavankumar656463@gmail.com>
2023-09-18 19:32:40 +0530

    Runnable file

--- command.py
+++ command.py
@@ -1,14 +1,3 @@
-"""
-satellite command system
-
-initial state :
-   orientation:North
-   solar panels :inactive
-   Data collected : 0
-
-"""
-
-
 class Satellite:
     def __init__(self):
         self.orientation = 'North'
@@ -16,9 +5,7 @@ class Satellite:
         self.DataCollected = 0
 
     def rotate(self, direction):
-        directions = ['North', 'South', 'East', 'West']
-        if direction in directions:
-            self.orientation = direction
+        self.orientation = direction
 
     def activatePanels(self):
         self.panels = True
@@ -30,9 +17,47 @@ class Satellite:
         if self.panels == True:
             self.DataCollected += 10
 
+    def get_status(self):
+        return [self.orientation , self.panels , self.DataCollected]
+
 
 if __name__ == '__main__':
     s = Satellite()
     while True:
+        function = None
+        direction = None
+        """
+        getting input from user 
+        ex : rotate(North) or rotate(South) 
+        """
         command = input(str())
-        print(command)
+        if command == '0' :
+            break
+        left  = command.find('(')
+        right = command.find(')')
+
+
+        if left != -1 and right != -1:
+            function = command[:left]
+            direction= command[left + 1:right]
+
+        if function == 'rotate':
+            directions = ['North', 'South', 'East', 'West']
+            if direction in directions:
+                s.rotate(direction)
+            print(f'Orientation: "{s.orientation}"')
+
+        if function == 'activatePanels':
+            s.activatePanels()
+            print(f'Solar Panels: "Active"')
+
+        if function == 'deactivatePanels':
+            s.activatePanels()
+            print(f'Solar Panels: "Unactive"')
+
+        if function == 'collectData':
+            s.collectData()
+            print(f'Data Collected: {s.DataCollected}')
+
+        if function == 'getStatus' or command =='':
+            print(f'{s.get_status()}')
